2946bf2e2cfd8aafaf0336481335624900db60c8
Ramazan Yavuz <info@yavuz.support>
2022-03-15 01:52:39 +0100

    Refactor app frontend and styles

--- sources/dev_web/cryptng-app/app/components/dashboard-test.js
+++ sources/dev_web/cryptng-app/app/components/dashboard-test.js
@@ -2,7 +2,8 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
-
+import { later } from '@ember/runloop';
+import { run } from '@ember/runloop'; 
 
 export default class Dashboard extends Component {
 
@@ -10,7 +11,7 @@ export default class Dashboard extends Component {
 
     @service web3service;
     @service router;
-  
+    //@service notify; //notify is broken for 4.x
     
 
 
@@ -40,6 +41,26 @@ export default class Dashboard extends Component {
     this.router.transitionTo('/');
   }
     
+  //ember notify does not work with 4.x this is a temporary workaround
+  @action mintWhenDisabled() {
+    let msg = "Minting is currently disabled."
+
+    var y = document.getElementById("toastdesc")
+    var x = document.getElementById("toast")
+    x.className = "show";
+    
+    setTimeout(function(){ y.textContent = msg; }, 800);
+    setTimeout(function(){ y.textContent = ""; }, 4200);
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 5000);
+}
+
+//notify is broken for 4.x
+// @action mintWhenDisabled()
+// {
+//   this.notify.alert(`Minting is currently disabled!`,{
+//     classNames: ['custom-notify']
+//   });
+// }
 
 
 }
