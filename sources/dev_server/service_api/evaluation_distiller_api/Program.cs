1f1c31091a2d5cac24025894ae4e6d026d9125d0
Ramazan Yavuz <info@yavuz.support>
2023-04-19 15:40:03 +0200

    PDF-Weaver: Enable SWAGGERUI always

--- sources/dev_server/service_api/evaluation_distiller_api/Program.cs
+++ sources/dev_server/service_api/evaluation_distiller_api/Program.cs
@@ -45,11 +45,11 @@ var app = builder.Build();
 
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseSession();
 
